e7bf43e610875b9a64fde058ab7ec6a30d12ca31
relogu <lollonasi97@gmail.com>
2023-08-13 12:29:50 +0100

    refractoring model initialisation

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
 from copy import deepcopy
 from typing import Iterable, Tuple
 
+import numpy as np
 import torch
 import torch.nn.functional as F
-from powerprop_modules import PowerPropConv2D, init_weights
+from powerprop_modules import PowerPropConv2D, PowerPropLinear
+from swat_modules import SWATConv2D, SWATLinear
 from torch import nn
 from torchvision.models import resnet18
 
@@ -42,40 +44,143 @@ class Net_Cifar_Resnet18(nn.Module):
         # self.net = resnet18(num_classes=10, norm_layer=lambda x: nn.GroupNorm(2, x))
         self.net = resnet18(num_classes=self.num_classes)
         # replace w/ smaller input layer
-        self.net.conv1 = torch.nn.Conv2d(
+        self.net.conv1 = nn.Conv2d(
             3, 64, kernel_size=3, stride=1, padding=1, bias=False
         )
         nn.init.kaiming_normal_(
             self.net.conv1.weight, mode="fan_out", nonlinearity="relu"
         )
         # no need for pooling if training for CIFAR-10
-        self.net.maxpool = torch.nn.Identity()
+        self.net.maxpool = nn.Identity()
 
     def forward(self, x):
         return self.net(x)
 
 
-def replace_conv_with_powerprop(
+def init_weights(module: nn.Module):
+    """Initialise PowerPropLinear and PowerPropConv2D layers in the input module."""
+    if isinstance(
+        module,
+        (
+            PowerPropLinear,
+            PowerPropConv2D,
+            SWATLinear,
+            SWATConv2D,
+            nn.Linear,
+            nn.Conv2d,
+        ),
+    ):
+        fan_in = calculate_fan_in(module.weight.data)
+
+        # constant from scipy.stats.truncnorm.std(a=-2, b=2, loc=0., scale=1.)
+        distribution_stddev = 0.87962566103423978
+
+        std = np.sqrt(1.0 / fan_in) / distribution_stddev
+        a, b = -2.0 * std, 2.0 * std
+
+        u = nn.init.trunc_normal_(module.weight.data, std=std, a=a, b=b)
+        if isinstance(
+            module,
+            (
+                PowerPropLinear,
+                PowerPropConv2D,
+                SWATLinear,
+                SWATConv2D,
+            ),
+        ):
+            u = torch.sign(u) * torch.pow(torch.abs(u), 1.0 / module.alpha)
+
+        module.weight.data = u
+        if module.bias is not None:
+            module.bias.data.zero_()
+
+
+def calculate_fan_in(tensor: torch.Tensor) -> float:
+    """Modified from: https://github.com/pytorch/pytorch/blob/master/torch/nn/init.py"""
+    dimensions = tensor.dim()
+    if dimensions < 2:
+        raise ValueError(
+            "Fan in can not be computed for tensor with fewer than 2 dimensions"
+        )
+
+    num_input_fmaps = tensor.size(1)
+    receptive_field_size = 1
+    if dimensions > 2:
+        for s in tensor.shape[2:]:
+            receptive_field_size *= s
+    fan_in = num_input_fmaps * receptive_field_size
+
+    return float(fan_in)
+
+
+def replace_layer_with_powerprop(
     module: nn.Module,
     name: str = "Model",
     alpha: float = 2.0,
 ):
-    """Replace every torch.nn.Conv2d layer with PowerPropConv2D"""
+    """Replace every nn.Conv2d and nn.Linear layers with the PowerProp versions."""
     for attr_str in dir(module):
         target_attr = getattr(module, attr_str)
-        if type(target_attr) == torch.nn.Conv2d:
+        if type(target_attr) == nn.Conv2d:
             new_conv = PowerPropConv2D(
                 alpha=alpha,
                 in_channels=target_attr.in_channels,
                 out_channels=target_attr.out_channels,
                 kernel_size=target_attr.kernel_size[0],
+                bias=target_attr.bias is not None,
                 padding=target_attr.padding,
                 stride=target_attr.stride,
             )
             setattr(module, attr_str, new_conv)
+        if type(target_attr) == nn.Linear:
+            new_conv = PowerPropLinear(
+                alpha=alpha,
+                in_features=target_attr.in_features,
+                out_features=target_attr.out_features,
+                bias=target_attr.bias is not None,
+            )
+            setattr(module, attr_str, new_conv)
 
     for name, immediate_child_module in module.named_children():
-        replace_conv_with_powerprop(immediate_child_module, name, alpha)
+        replace_layer_with_powerprop(immediate_child_module, name, alpha)
+
+
+def replace_layer_with_swat(
+    module: nn.Module,
+    name: str = "Model",
+    alpha: float = 2.0,
+    sparsity: float = 0.3,
+):
+    """Replace every nn.Conv2d and nn.Linear layers with the SWAT versions."""
+    for attr_str in dir(module):
+        target_attr = getattr(module, attr_str)
+        if type(target_attr) == nn.Conv2d:
+            new_conv = SWATConv2D(
+                alpha=alpha,
+                in_channels=target_attr.in_channels,
+                out_channels=target_attr.out_channels,
+                kernel_size=target_attr.kernel_size[0],
+                bias=target_attr.bias is not None,
+                padding=target_attr.padding,
+                stride=target_attr.stride,
+                sparsity=sparsity,
+                pruning_type="unstructured",
+                warm_up=0,
+                period=1,
+            )
+            setattr(module, attr_str, new_conv)
+        if type(target_attr) == nn.Linear:
+            new_conv = SWATLinear(
+                alpha=alpha,
+                in_features=target_attr.in_features,
+                out_features=target_attr.out_features,
+                bias=target_attr.bias is not None,
+                sparsity=sparsity,
+            )
+            setattr(module, attr_str, new_conv)
+
+    for name, immediate_child_module in module.named_children():
+        replace_layer_with_swat(immediate_child_module, name, alpha)
 
 
 def get_parameters_to_prune(
@@ -86,13 +191,19 @@ def get_parameters_to_prune(
 
     def add_immediate_child(
         module: nn.Module,
+        name: str,
     ) -> None:
-        if type(module) == PowerPropConv2D:
-            parameters_to_prune.append((module, "w"))
-        for _, immediate_child_module in module.named_children():
-            add_immediate_child(immediate_child_module)
+        if (
+            type(module) == PowerPropConv2D
+            or type(module) == nn.Conv2d
+            or type(module) == nn.Linear
+            or type(module) == PowerPropLinear
+        ):
+            parameters_to_prune.append((module, "w", name))
+        for name, immediate_child_module in module.named_children():
+            add_immediate_child(immediate_child_module, name)
 
-    add_immediate_child(net)
+    add_immediate_child(net, "Net")
 
     return parameters_to_prune
 
@@ -119,10 +230,36 @@ def get_network_generator_resnet() -> Net_Cifar_Resnet18:
 
 def get_network_generator_resnet_powerprop(alpha: float = 2.0) -> Net_Cifar_Resnet18:
     untrained_net: Net_Cifar_Resnet18 = Net_Cifar_Resnet18(num_classes=10)
-    replace_conv_with_powerprop(
+    replace_layer_with_powerprop(
         module=untrained_net,
         name="Net_Cifar_Resnet18",
         alpha=alpha,
+    )
+
+    def init_model(
+        module: nn.Module,
+    ) -> None:
+        init_weights(module)
+        for _, immediate_child_module in module.named_children():
+            init_model(immediate_child_module)
+
+    init_model(untrained_net)
+
+    def generated_net() -> Net_Cifar_Resnet18:
+        return deepcopy(untrained_net)
+
+    return generated_net
+
+
+def get_network_generator_resnet_swat(
+    alpha: float = 2.0, sparsity: float = 0.3
+) -> Net_Cifar_Resnet18:
+    untrained_net: Net_Cifar_Resnet18 = Net_Cifar_Resnet18(num_classes=10)
+    replace_layer_with_swat(
+        module=untrained_net,
+        name="Net_Cifar_Resnet18",
+        alpha=alpha,
+        sparsity=sparsity,
     )
 
     def init_model(
--- powerprop_modules.py
+++ powerprop_modules.py
@@ -8,48 +8,10 @@ Adapted from the code available at: https://github.com/mysistinechapel/powerprop
 
 from typing import Union
 
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.types import _int, _size
-
-
-def init_weights(module: nn.Module):
-    """Initialise PowerPropLinear and PowerPropConv2D layers in the input module."""
-    if isinstance(module, (PowerPropLinear, PowerPropConv2D)):
-        fan_in = calculate_fan_in(module.w.data)
-
-        # constant from scipy.stats.truncnorm.std(a=-2, b=2, loc=0., scale=1.)
-        distribution_stddev = 0.87962566103423978
-
-        std = np.sqrt(1.0 / fan_in) / distribution_stddev
-        a, b = -2.0 * std, 2.0 * std
-
-        u = nn.init.trunc_normal_(module.w.data, std=std, a=a, b=b)
-        u = torch.sign(u) * torch.pow(torch.abs(u), 1.0 / module.alpha)
-
-        module.w.data = u
-        if module.b is not None:
-            module.b.data.zero_()
-
-
-def calculate_fan_in(tensor: torch.Tensor) -> float:
-    """Modified from: https://github.com/pytorch/pytorch/blob/master/torch/nn/init.py"""
-    dimensions = tensor.dim()
-    if dimensions < 2:
-        raise ValueError(
-            "Fan in can not be computed for tensor with fewer than 2 dimensions"
-        )
-
-    num_input_fmaps = tensor.size(1)
-    receptive_field_size = 1
-    if dimensions > 2:
-        for s in tensor.shape[2:]:
-            receptive_field_size *= s
-    fan_in = num_input_fmaps * receptive_field_size
-
-    return float(fan_in)
 
 
 class PowerPropLinear(nn.Module):
@@ -66,25 +28,25 @@ class PowerPropLinear(nn.Module):
         self.alpha = alpha
         self.in_features = in_features
         self.out_features = out_features
-        self.bias = bias
-        self.w = torch.nn.Parameter(torch.empty(out_features, in_features))
-        self.b = torch.nn.Parameter(torch.empty(out_features)) if self.bias else None
+        self.b = bias
+        self.weight = nn.Parameter(torch.empty(out_features, in_features))
+        self.bias = nn.Parameter(torch.empty(out_features)) if self.b else None
 
     def __repr__(self):
-        return f"PowerPropLinear(alpha={self.alpha}, in_features={self.in_features}, out_features={self.out_features}, bias={self.bias})"
+        return f"PowerPropLinear(alpha={self.alpha}, in_features={self.in_features}, out_features={self.out_features}, bias={self.b})"
 
-    def get_weights(self):
-        weights = self.w.detach()
-        return torch.sign(weights) * torch.pow(torch.abs(weights), self.alpha)
+    def get_weight(self):
+        weight = self.weight.detach()
+        return torch.sign(weight) * torch.pow(torch.abs(weight), self.alpha)
 
     def forward(self, inputs, mask=None):
-        # Apply the re-parametrisation to `self.w` using `self.alpha`
-        weights = self.w * torch.pow(torch.abs(self.w), self.alpha - 1.0)
+        # Apply the re-parametrisation to `self.weight` using `self.alpha`
+        weight = self.weight * torch.pow(torch.abs(self.weight), self.alpha - 1.0)
         # Apply a mask, if given
         if mask is not None:
-            weights *= mask
-        # Compute the linear forward pass usign the re-parametrised weights
-        return F.linear(inputs=inputs, weights=weights, bias=self.b)
+            weight *= mask
+        # Compute the linear forward pass usign the re-parametrised weight
+        return F.linear(inputs=inputs, weight=weight, bias=self.bias)
 
 
 class PowerPropConv2D(nn.Module):
@@ -107,34 +69,34 @@ class PowerPropConv2D(nn.Module):
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
-        self.bias = bias
-        self.w = torch.nn.Parameter(
+        self.b = bias
+        self.weight = nn.Parameter(
             torch.empty(out_channels, in_channels, kernel_size, kernel_size)
         )
-        self.b = torch.nn.Parameter(torch.empty(out_channels)) if bias else None
+        self.bias = nn.Parameter(torch.empty(out_channels)) if bias else None
         self.stride = stride
         self.padding = padding
         self.dilation = dilation
         self.groups = groups
 
     def __repr__(self):
-        return f"PowerPropConv2D(alpha={self.alpha}, in_channels={self.in_channels}, out_channels={self.out_channels}, kernel_size={self.kernel_size}, bias={self.bias}, stride={self.stride}, padding={self.padding}, dilation={self.dilation}, groups={self.groups})"
+        return f"PowerPropConv2D(alpha={self.alpha}, in_channels={self.in_channels}, out_channels={self.out_channels}, kernel_size={self.kernel_size}, bias={self.b}, stride={self.stride}, padding={self.padding}, dilation={self.dilation}, groups={self.groups})"
 
     def get_weights(self):
-        weights = self.w.detach()
+        weights = self.weight.detach()
         return torch.sign(weights) * torch.pow(torch.abs(weights), self.alpha)
 
     def forward(self, inputs, mask=None):
-        # Apply the re-parametrisation to `self.w` using `self.alpha`
-        weights = self.w * torch.pow(torch.abs(self.w), self.alpha - 1.0)
+        # Apply the re-parametrisation to `self.weight` using `self.alpha`
+        weight = self.weight * torch.pow(torch.abs(self.weight), self.alpha - 1.0)
         # Apply a mask, if given
         if mask is not None:
-            weights *= mask
-        # Compute the conv2d forward pass usign the re-parametrised weights
+            weight *= mask
+        # Compute the conv2d forward pass usign the re-parametrised weight
         return F.conv2d(
             input=inputs,
-            weight=weights,
-            bias=self.b,
+            weight=weight,
+            bias=self.bias,
             stride=self.stride,
             padding=self.padding,
             dilation=self.dilation,
